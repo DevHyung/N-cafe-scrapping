6b27f880b52212f5b4a759480c2449cd5ba41d6f
DevHyung <khuphj@gmail.com>
2018-09-04 01:13:02 +0900

    Debug 용

--- UTIL.py
+++ UTIL.py
@@ -41,3 +41,29 @@ def save_excel(_FILENAME, _DATA, _HEADER):
 def get_bs_by_txt(_FILENAME):
     with open(_FILENAME,'r',encoding='utf8') as f:
         return BeautifulSoup(f.read(),'lxml')
+
+cafeIdList = [585,586,392,418,590,591,592,242,126]
+titleList = ['임신준비 질문방', '테스터/초음파 질문방', '임신 중 질문방', '난임/인공/시험관 질문방', '분만 질문방', '산후조리 질문방', '산후다이어트 질문방', '수유 질문방', '육아 질문방']
+linkList = [
+    'https://cafe.naver.com/imsanbu/ArticleList.nhn?search.clubid=10094499&search.menuid=585&search.boardtype=L',
+    'https://cafe.naver.com/imsanbu/ArticleList.nhn?search.clubid=10094499&search.menuid=586&search.boardtype=L',
+    'https://cafe.naver.com/imsanbu/ArticleList.nhn?search.clubid=10094499&search.menuid=392&search.boardtype=L',
+    'https://cafe.naver.com/imsanbu/ArticleList.nhn?search.clubid=10094499&search.menuid=418&search.boardtype=L',
+    'https://cafe.naver.com/imsanbu/ArticleList.nhn?search.clubid=10094499&search.menuid=590&search.boardtype=L',
+    'https://cafe.naver.com/imsanbu/ArticleList.nhn?search.clubid=10094499&search.menuid=591&search.boardtype=L',
+    'https://cafe.naver.com/imsanbu/ArticleList.nhn?search.clubid=10094499&search.menuid=592&search.boardtype=L',
+    'https://cafe.naver.com/imsanbu/ArticleList.nhn?search.clubid=10094499&search.menuid=242&search.boardtype=L',
+    'https://cafe.naver.com/imsanbu/ArticleList.nhn?search.clubid=10094499&search.menuid=126&search.boardtype=L'
+    ]
+urlFormat = 'https://cafe.naver.com/ArticleSearchList.nhn?search.clubid=10094499&search.searchdate={}{}&search.searchBy=0&search.query=&search.defaultValue=1&search.sortBy=date&userDisplay=50&search.media=0&search.option=0&search.menuid={}&search.page={}'
+iframeUrl = 'https://cafe.naver.com/iframe_url=/imsanbu'
+if __name__ =="__main__":
+    FILENAME = 'result.xlsx'
+    HEADER = ['글번호', '작성일', '작성자', '제목', '내용', '답글1', '답글2', '답글3', '답글4', '답글5']
+    save_excel(FILENAME,None,HEADER)
+
+    bs4 = get_bs_by_txt('html.txt')
+    trs = bs4.find('div',class_='article-board m-tcol-c').find_all('tr',align='center')
+    for tr in trs:
+        print(iframeUrl+tr.a['href'],tr.a.get_text().strip())
+    #print(bs4.prettify())
